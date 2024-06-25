44dce75f8036f4ec25c392e84603c4844b1a4d99
acyhuang <allisoncyhuang@gmail.com>
2024-06-24 18:16:06 -0700

    Tested Supabase connection

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SupabaseTest from './test.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Supabase React App</h1>
+      <SupabaseTest />
+    </div>
+  );
+}
+
+export default App;
